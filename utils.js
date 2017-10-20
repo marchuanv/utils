ed56695eea12d9a07add3081aa88d89d55e4317b
marchuanv <Marchuanv@gmail.com>
2017-10-20 12:51:27 +0200

    Update utils.js

--- utils.js
+++ utils.js
@@ -1,5 +1,23 @@
+function Timer(isInterval){
+  this.seconds=1000;
+  this.start=function(callback){
+      if (isInterval){
+         setTimeout.apply(this, function(){
+            callback.apply(this);
+            this.start(callback);
+          }, this.seconds);
+      } else {
+        setTimeout.apply(this, function(){
+          callback.apply(this);
+        }, this.seconds);
+      }
+  }
+};
 module.exports={
-  getRandomNumber:function(min, max){
+  getRandomNumber: function(min, max){
     return Math.floor(Math.random()*(max-min+1)+min);
-  };
+  },
+  createTimer: function(isInterval){
+    return new Timer(isInterval);
+  }
 };

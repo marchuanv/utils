01243a06f09eb78c5115512a853e0d7b7e858987
marchuanv <Marchuanv@gmail.com>
2017-10-23 13:01:58 +0200

    Update utils.js

--- utils.js
+++ utils.js
@@ -19,6 +19,18 @@ function Timer(isInterval){
       internalStart.apply(this, [callback]);
   };
 };
+function getHostAndPortFromUrl(url){
+    const addressSplit=url.replace('http://','').replace('https://','').split(':');
+    const hostName = addressSplit[0].split('/')[0];
+    var port=80;
+    if (addressSplit[1]){
+        port = addressSplit[1].split('/')[0];
+    }
+    return {
+        host: hostName,
+        port: port
+    };
+};
 module.exports={
   getRandomNumber: function(min, max){
     return Math.floor(Math.random()*(max-min+1)+min);

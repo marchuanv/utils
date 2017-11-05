61884e8fffd9dbcacba37904298bbc8c604a02e6
marchuanv <Marchuanv@gmail.com>
2017-11-05 11:57:36 +0200

    Update logging.js

--- logging.js
+++ logging.js
@@ -1,14 +1,28 @@
-function Logging(){
-	this.write=function(message, args){
-		if (message==undefined || message==null){
-			 var err = new Error('message was empty');
-			throw err;
-		}
-		if (args){
-			console.log(message, args);
-		}else{
-			console.log(message);
-		}
-	};
+function Logging() {
+    var conditions = [];
+    this.condition = function(cond) {
+        conditions.push(cond);
+    }
+    this.write = function(message, args) {
+        if (message == undefined || message == null) {
+            var err = new Error('message was empty');
+            throw err;
+        }
+        var passed = true;
+        for (var i = 0; i < conditions.lenght; i++) {
+            const cond = conditions[i];
+            if (cond() == false) {
+                passed = false;
+            }
+        };
+        if (passed == false) {
+            return;
+        }
+        if (args) {
+            console.log(message, args);
+        } else {
+            console.log(message);
+        }
+    };
 };
-module.exports=new Logging();
+module.exports = new Logging();

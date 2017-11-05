c7205faf0583f98d1b13739b04db82abf03b4f55
marchuanv <Marchuanv@gmail.com>
2017-11-05 20:48:42 +0200

    Create events.js

---
+++ events.js
@@ -0,0 +1,24 @@
+var google = require('googleapis');
+var key = require('./privatekey.json');
+var authScopes =  ['https://www.googleapis.com/auth/drive','https://www.googleapis.com/auth/drive.file'];
+
+var jwtClient = new google.auth.JWT(
+  key.client_email,
+  null,
+  key.private_key,
+  authScopes, // an array of auth scopes
+  null
+);
+
+jwtClient.authorize(function (err, tokens) {
+  if (err) {
+    console.log(err);
+    return;
+  }
+  // Make an authorized request to list Drive files.
+  drive.files.list({
+    auth: jwtClient
+  }, function (err, resp) {
+    // handle err and response
+  });
+});

7edae6ae370f771ac709a050349db44e7c751f74
Marchuan Van der Merwe <marchuanv@sahomeloans.com>
2017-11-08 11:47:00 +0200

    fix

--- logging.js
+++ logging.js
@@ -1,26 +1,13 @@
 function Logging() {
-    var conditions = [];
-    this.condition = function(cond) {
-        conditions.push(cond);
-    }
     this.write = function(message, args) {
         if (message == undefined || message == null) {
             var err = new Error('message was empty');
             throw err;
         }
         if(typeof message!=='string'){
- console.error(' MESSAGE WAS NOT A STRING',message);
-}
-        var passed = true;
-        for (var i = 0; i < conditions.length; i++) {
-            const cond = conditions[i];
-            if (cond(message) == false) {
-                passed = false;
-            }
-        };
-        if (passed == false) {
-            return;
+            console.error(' MESSAGE WAS NOT A STRING',message);
         }
+       
         if (args) {
             console.log(message, args);
         } else {

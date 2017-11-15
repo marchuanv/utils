28881eec143cb4acd4e7ee3c6cff6ff58fee51a6
marchuanv <Marchuanv@gmail.com>
2017-11-15 16:02:14 +0200

    Update messageBus.js

--- messageBus.js
+++ messageBus.js
@@ -1,7 +1,7 @@
 const utils = require('./utils.js');
 const logging = require('./logging.js');
 
-function MessageBus(messageBusService, serviceFileName, canReplay){
+function MessageBus(messageBusService, canReplay){
 
 	const thisService=this;
 	var subscriptions=[];

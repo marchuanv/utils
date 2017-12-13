1346fc9b03fe0fa663402d2cd1ef7c82ebc5732b
Marchuan Van Der Merwe <marchuanv@sahomeloans.com>
2017-12-13 10:50:45 +0200

    saved

---
+++ httpService.js
@@ -0,0 +1,98 @@
+function HttpService(http, utils){
+	this.sendHttpRequest=function (url, data, path, callback, callbackFail) {
+		console.log('creating an http request.');
+		const postData=utils.getJSONString(data);
+		const info = utils.getHostAndPortFromUrl(url);
+		const host=info.host;
+		var port=info.port;
+		if (!port){
+			port=80;
+		}
+		const options = {
+			host: host,
+			port: port, 
+			method:'POST',
+			path: path,
+			headers: {
+			  'Content-Type': 'application/json',
+			  'Content-Length': Buffer.byteLength(postData)
+			}
+		};
+		const request=http.request(options);
+		request.on('error', function(err){
+			if (callbackFail){
+				callbackFail(err);
+			}else{
+				console.log(`Http error occurred: ${err}`);
+			}
+		});
+		request.on('response', function (response) {
+		  response.setEncoding('utf8');
+		  if (response.statusCode != 200){
+		      if (callbackFail){
+		        callbackFail(`http request responded with http status code: ${response.statusCode}`);
+		      }else{
+		        console.log('http response received from request, status code: ', response.statusCode);
+		      }
+		  }else{
+		    response.on('data', function (body) {
+		        if (callback){
+		          callback(body);
+		        }
+		    });
+		  }
+		});
+		request.end(postData);
+	};
+    this.receiveHttpRequest=function(hostPort, cbStarted, responseCallback, callbackError){
+		const port = process.env.PORT || hostPort;
+		const httpServer=http.createServer(function(req, res){
+			console.log('http request received');
+			const body = [];
+			res.on('error',function(err){
+			  const errMsg=`Http error occurred at ${location}: ${err}`;
+			  console.log(errMsg);
+			  callbackError(errMsg);
+			});
+			req.on('data', function (chunk) {
+				body.push(chunk);
+			});
+			req.on('end', function () {
+			    const requestBodyJson=Buffer.concat(body).toString();
+			    const requestBody=utils.getJSONObject(requestBodyJson);
+			    console.log('http request data received.');
+			    if (requestBody) {
+			        res.statusCode = 200;
+			        res.end('success');
+			        responseCallback(requestBody);
+			    } else if(req.method.toLowerCase()=="get"){
+			        responseCallback(function(resData){
+			            res.statusCode = 200;
+			            var resDataJson=utils.getJSONString(resData);
+			            res.end(resDataJson);
+			        });
+			    }else{
+			        res.statusCode = 500;
+			        res.end('failed');
+			        callbackError(`no request body`);
+			    }
+			});
+		});
+      	try{
+        	httpServer.listen(port, function(){
+            	console.log('');
+            	console.log(`http server started and listening on port ${port}`);
+            	console.log('');
+            	cbStarted();
+        	});
+      	}catch(err){
+			console.log('MESSAGE: ',err.message);
+			if (err.message.indexOf('EADDRINUSE') == -1){
+			  throw err;
+			}else{
+			  console.error(`port ${hostPort} already in use`);
+			}
+      	}
+    };
+};
+module.exports=HttpService;
--- utils.js
+++ utils.js
@@ -1,4 +1,3 @@
-const http=require('http');
 const logging=require('./logging.js');
 
 module.exports={
@@ -58,154 +57,57 @@ module.exports={
           port: port
       };
   },
-  sendHttpRequest: function(url, data, path, callback, callbackFail){
-      logging.write('creating an http request.');
-      const postData=module.exports.getJSONString(data);
-      const info = module.exports.getHostAndPortFromUrl(url);
-      const host=info.host;
-      var port=info.port;
-      if (!port){
-        port=80;
-      }
-      const options = {
-          host: host,
-          port: port, 
-          method:'POST',
-          path: path,
-          headers: {
-              'Content-Type': 'application/json',
-              'Content-Length': Buffer.byteLength(postData)
-          }
-      };
-      const request=http.request(options);
-      request.on('error', function(err){
-          if (callbackFail){
-              callbackFail(err);
-          }else{
-              logging.write(`Http error occurred: ${err}`);
-          }
+  isValidUrl: function(url){
+      var pattern = new RegExp('^(https?:\\/\\/)?'+ // protocol
+        '((([a-z\\d]([a-z\\d-]*[a-z\\d])*)\\.?)+[a-z]{2,}|'+ // domain name
+        '((\\d{1,3}\\.){3}\\d{1,3}))'+ // OR ip (v4) address
+        '(\\:\\d+)?(\\/[-a-z\\d%_.~+]*)*'+ // port and path
+        '(\\?[;&a-z\\d%_.~+=-]*)?'+ // query string
+        '(\\#[-a-z\\d_]*)?$','i'); // fragment locator
+      return pattern.test(url);
+  },
+  getUrlPath(url){
+      var url_parts = require('url').parse(url);
+      return url_parts.pathname;
+  },
+  createCache: function(cacheString){
+      const Cache=require('./cache.js');
+      return new Cache(cacheString);
+  },
+  createLogging: function(){
+    return require('./logging.js');
+  },
+  readJsonFile: function(name, callback){
+      var filePath=`${__dirname}/${name}`;
+      filePath=filePath.replace('.json','');
+      filePath=`${filePath}.json`;
+      filePath=filePath.replace('node_modules/utils/','');
+      const fs=require('fs');
+      fs.readFile(filePath, 'utf8', function(err, jsonStr){
+          const data=module.exports.getJSONObject(jsonStr);
+          callback(data);
       });
-      request.on('response', function (response) {
-          response.setEncoding('utf8');
-          if (response.statusCode != 200){
-              if (callbackFail){
-                callbackFail(`http request responded with http status code: ${response.statusCode}`);
-              }else{
-                logging.write('http response received from request, status code: ', response.statusCode);
-              }
-          }else{
-            response.on('data', function (body) {
-                if (callback){
-                  callback(body);
-                }
-            });
-          }
-      });
-      request.end(postData);
-    },
-    receiveHttpRequest: function(hostPort, callback, callbackError){
-      const port = process.env.PORT || hostPort;
-      const httpServer=http.createServer(function(req, res){
-          logging.write('http request received');
-          const body = [];
-          res.on('error',function(err){
-              const errMsg=`Http error occurred at ${location}: ${err}`;
-              logging.write(errMsg);
-              callbackError(errMsg);
-          });
-          req.on('data', function (chunk) {
-              body.push(chunk);
-          });
-          req.on('end', function () {
-            const requestBodyJson=Buffer.concat(body).toString();
-            const requestBody=module.exports.getJSONObject(requestBodyJson);
-            logging.write('http request data received.');
-                if (requestBody) {
-                    res.statusCode = 200;
-                    res.end('success');
-                    callback(requestBody);
-                } else if(req.method.toLowerCase()=="get"){
-                    callback(function(resData){
-                        res.statusCode = 200;
-                        var resDataJson=module.exports.getJSONString(resData);
-                        res.end(resDataJson);
-                    });
-                }else{
-                    res.statusCode = 500;
-                    res.end('failed');
-                    callbackError(`no request body`);
-                }
-            
-        });
-      });
-        
-      try{
-        httpServer.listen(port, function(){
-            logging.write('');
-            logging.write(`http server started and listening on port ${port}`);
-            logging.write('');
-        });
-      }catch(err){
-        
-        console.log('MESSAGE: ',err.message);
-        if (err.message.indexOf('EADDRINUSE') == -1){
-          throw err;
-        }else{
-          console.error(`port ${hostPort} already in use`);
-        }
-      }
-    },
-    isValidUrl: function(url){
-        var pattern = new RegExp('^(https?:\\/\\/)?'+ // protocol
-          '((([a-z\\d]([a-z\\d-]*[a-z\\d])*)\\.?)+[a-z]{2,}|'+ // domain name
-          '((\\d{1,3}\\.){3}\\d{1,3}))'+ // OR ip (v4) address
-          '(\\:\\d+)?(\\/[-a-z\\d%_.~+]*)*'+ // port and path
-          '(\\?[;&a-z\\d%_.~+=-]*)?'+ // query string
-          '(\\#[-a-z\\d_]*)?$','i'); // fragment locator
-        return pattern.test(url);
-    },
-    getUrlPath(url){
-        var url_parts = require('url').parse(url);
-        return url_parts.pathname;
-    },
-    createCache: function(cacheString){
-        const Cache=require('./cache.js');
-        return new Cache(cacheString);
-    },
-    createLogging: function(){
-      return require('./logging.js');
-    },
-    readJsonFile: function(name, callback){
-        var filePath=`${__dirname}/${name}`;
-        filePath=filePath.replace('.json','');
-        filePath=`${filePath}.json`;
-        filePath=filePath.replace('node_modules/utils/','');
-        const fs=require('fs');
-        fs.readFile(filePath, 'utf8', function(err, jsonStr){
-            const data=module.exports.getJSONObject(jsonStr);
-            callback(data);
-        });
-    },
-    replaceJsonFile: function(name, data){
-        var filePath=`${__dirname}/${name}`;
-        filePath=filePath.replace('.json','');
-        filePath=`${filePath}.json`;
-        filePath=filePath.replace('node_modules/utils/','');
-        const fs=require('fs');
-        fs.unlink(filePath, function(err) {
-           // Ignore error if no file already exists	 
-           if (err && err.code !== 'ENOENT') {
-              throw err;
-           }
-           const dataStr = module.exports.getJSONString(data);
-           var options = {
-              flag: 'w'
-           };
-           fs.writeFile(filePath, dataStr, options, function(err) {
-              if (err) {
-                 throw err;
-              }
-           });
+  },
+  replaceJsonFile: function(name, data){
+      var filePath=`${__dirname}/${name}`;
+      filePath=filePath.replace('.json','');
+      filePath=`${filePath}.json`;
+      filePath=filePath.replace('node_modules/utils/','');
+      const fs=require('fs');
+      fs.unlink(filePath, function(err) {
+         // Ignore error if no file already exists	 
+         if (err && err.code !== 'ENOENT') {
+            throw err;
+         }
+         const dataStr = module.exports.getJSONString(data);
+         var options = {
+            flag: 'w'
+         };
+         fs.writeFile(filePath, dataStr, options, function(err) {
+            if (err) {
+               throw err;
+            }
          });
-      }
+       });
+    }
 };

060b0a79f8dba6852ef0d544a2074ec79c36b8d8
marchuanv <Marchuanv@gmail.com>
2018-02-20 17:03:29 +0200

    Update utils.js

--- utils.js
+++ utils.js
@@ -1,6 +1,12 @@
 const logging=require('./logging.js');
 
 module.exports={
+  generateGUID: function(argument) {
+      function S4() {
+          return (((1 + Math.random()) * 0x10000) |0).toString(16).substring(1); 
+      }
+      return (S4() + S4() + "-" + S4() + "-4" + S4().substr(0, 3) + "-" + S4() + "-" + S4() + S4() + S4()).toLowerCase();
+  },
   getFunctionName: function(func) {
     var ret = func.toString();
     ret = ret.substr('function '.length);

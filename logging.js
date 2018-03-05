2294019638b871bd7ff46e0d41e418c1e3b2cc76
Marchuan Van Der Merwe <marchuanv@sahomeloans.com>
2018-03-05 13:29:35 +0200

    fix

--- logging.js
+++ logging.js
@@ -1,4 +1,14 @@
 function Logging() {
+    function generateGUID(argument) {
+      function S4() {
+          return (((1 + Math.random()) * 0x10000) |0).toString(16).substring(1); 
+      }
+      return (S4() + S4() + "-" + S4() + "-4" + S4().substr(0, 3) + "-" + S4() + "-" + S4() + S4() + S4()).toLowerCase();
+    }
+    var logTrack='';
+    this.track=function(){
+        logTrack=generateGUID();
+    },
     this.write = function(message, args) {
         if (message == undefined || message == null) {
             var err = new Error('message was empty');
@@ -7,11 +17,11 @@ function Logging() {
         if(typeof message!=='string'){
             console.error(' MESSAGE WAS NOT A STRING',message);
         }
-       
+        var newMessage=`[${logTrack}]${message}`;
         if (args) {
-            console.log(message, args);
+            console.log(newMessage, args);
         } else {
-            console.log(message);
+            console.log(newMessage);
         }
     };
 };

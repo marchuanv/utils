b7ac114a5113ac8714e52b69ae369a07bdb9b4da
Marchuan Van Der Merwe <marchuanv@sahomeloans.com>
2018-04-09 12:45:36 +0200

    fix

--- utils.js
+++ utils.js
@@ -160,26 +160,5 @@ module.exports={
         callback(prop, obj[prop]);
       }
     };
-  },
-  isNodeApp: function () {
-
-    // Establish the root object, `window` in the browser, or `global` on the server.
-    var root = this; 
-
-    // Create a reference to this
-    var _ = new Object();
-
-    var isNode = false;
-
-    // Export the Underscore object for **CommonJS**, with backwards-compatibility
-    // for the old `require()` API. If we're not in CommonJS, add `_` to the
-    // global object.
-    if (typeof module !== 'undefined' && module.exports) {
-            module.exports = _;
-            root._ = _;
-            isNode = true;
-    } else {
-            root._ = _;
-    }
   }
 };

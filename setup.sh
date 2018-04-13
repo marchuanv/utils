c3acfe48878ab4191857cf67c9e7015fc3c35c48
Marchuan Van Der Merwe <marchuanv@sahomeloans.com>
2018-04-13 18:09:59 +0200

    fix

--- setup.sh
+++ setup.sh
@@ -1,13 +1,19 @@
+reset
+sudo fuser -k 3333/tcp
+export PORT
+PORT=3333
+cd ./../libraryhost/
+./setup.sh
+cd ./../utils/
 results=$(git status | grep "nothing to commit")
 if [ ! -z "$results" -a "$results" != " " ]; then
 	echo "nothing has changed moving on..."
 else
-	reset
 	git pull origin master
 	git add -A
 	git commit -m "fix"
 	git push origin master
-	git clean -fdx
-	npm install
-	npm update
 fi
+npm update
+reset
+npm start

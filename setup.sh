a4407e42febe2e12e5e13ba390a3d4331554fd9d
Marchuan Van Der Merwe <marchuanv@sahomeloans.com>
2018-04-13 13:52:35 +0200

    fix

--- setup.sh
+++ setup.sh
@@ -1,8 +1,13 @@
-reset
-git pull origin master
-git add -A
-git commit -m "fix"
-git push origin master
-git clean -fdx
-npm install
-npm update
+results=$(git status | grep "nothing to commit")
+if [ ! -z "$results" -a "$results" != " " ]; then
+	echo "nothing has changed moving on..."
+else
+	reset
+	git pull origin master
+	git add -A
+	git commit -m "fix"
+	git push origin master
+	git clean -fdx
+	npm install
+	npm update
+fi

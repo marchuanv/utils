092cb824a5dc806669b1822fef66a13d14cb3a1f
Marchuan Van Der Merwe <marchuanv@sahomeloans.com>
2018-04-18 16:21:40 +0200

    fix

--- setup.sh
+++ setup.sh
@@ -1,3 +1,14 @@
+function waitForDirectory(){
+	busy=$(lsof +D $1) | grep PID
+	if [ -z "$busy" ]
+	then
+		echo "$1 directory not in use moving on..."
+	else
+		echo "waiting for processes using $1"
+	    sleep 2
+	fi
+}
+
 reset
 sudo fuser -k 3002/tcp
 sudo fuser -k 3002/tcp
@@ -6,27 +17,59 @@ export PORT
 PORT=3002
 
 export libraryregisters
-libraryregisters='http://localhost:3000/registers'
+libraryregisters='http://tidal:3000/registers'
 
 cd ./../libraryhost/
 ./setup.sh
+
 cd ./../utils/
+
+echo ""
+echo "SETTING UP UTILS"
+echo ""
+
+if [ -d "./node_modules" ] 
+then
+    echo "dependencies already installed moving on..." 
+else
+	echo "installing dependencies"
+	npm install
+fi
+
 results=$(git status | grep "nothing to commit")
 if [ ! -z "$results" -a "$results" != " " ]; then
 	echo "nothing has changed moving on..."
-	mkdir ./../working
-	mv node_modules/.[!.]* ./../working
-	git clean -fdx
-	mkdir node_modules
-	mv ./../working/.[!.]* node_modules
-	rmdir ./../working
 else
 	git pull origin master
 	git add -A
 	git commit -m "fix"
 	git push origin master
+	npm install
 fi
-npm update
+
+mkdir -p ./../working
+
+mv ./node_modules/* ./../working
+waitForDirectory ./../working
+waitForDirectory node_modules/
+
+mv ./node_modules/.* ./../working
+waitForDirectory ./../working
+waitForDirectory node_modules/
+
+git clean -fdx
+mkdir ./node_modules
+
+mv ./../working/* ./node_modules/
+waitForDirectory ./../working
+waitForDirectory node_modules/
+
+mv ./../working/.* ./node_modules/
+waitForDirectory ./../working
+waitForDirectory node_modules/
+
+npm update libraryhost
+
 reset
 echo ""
 (nohup npm start /dev/null 2>&1 &)

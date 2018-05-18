640f464dedcced11fbee3c45d09530bdf4cf18ce
Marchuan Van Der Merwe <marchuanv@sahomeloans.com>
2018-05-18 13:11:29 +0200

    automated commit

--- commit.ps1
+++ commit.ps1
@@ -1,8 +1,13 @@
-Param (
-    [string]$moduleName
-)
 . .\shared.ps1
+$moduleName = Get-Location 
 Write-Host ""
-Write-Host "COMMITTING $moduleName CHANGES"
 [bool]$results= CommitAndPush-GitRepository $moduleName
-exit $results
+if ($results -eq $true){
+    Write-Host "COMMITTING $moduleName CHANGES"
+    $LASTEXITCODE=$true
+    exit 0
+}else{
+    Write-Host "NOTHING TO COMMITTING FOR $moduleName"
+    $LASTEXITCODE=$false
+    exit 0
+}

c98ce1576808f3d57aa2016a98a0b1338e927756
Marchuan Van Der Merwe <marchuanv@sahomeloans.com>
2018-05-21 13:37:56 +0200

    automated commit

--- install.ps1
+++ install.ps1
@@ -1,8 +1,6 @@
-param($depModuleName)
 $package=Load-NodePackage
 $moduleName=$package.name
-$repoUrl=$package.dependencies."$depModuleName"
-Write-Host "installing and updating $moduleName with changes from $repoUrl"
-npm update $repoUrl
-npm install $repoUrl
+Write-Host "installing and updating $moduleName"
+npm install .
+npm update .
 $LASTEXITCODE=$true

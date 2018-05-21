3b07b86d730f6df35f02bffde2a1fdcbd6d9d6cd
Marchuan Van Der Merwe <marchuanv@sahomeloans.com>
2018-05-21 16:06:43 +0200

    automated commit

--- start.ps1
+++ start.ps1
@@ -1,16 +1,8 @@
-$package= Load-NodePackage
-$moduleName=$package.name
-
-$dependencies=Get-ObjectProperties $package.dependencies
-
-Write-Host $dependencies
-
-# $startFilePath="node_modules\$moduleName\$moduleName.start.js"
-# $startFilePath= Convert-Path $startFilePath
-# [bool]$exists=(Test-Path $startFilePath)
-# if ($exists -eq $false){
-#     $startFilePath="$moduleName.start.js"
-#     $startFilePath= Convert-Path $startFilePath
-# }
-# node $startFilePath
+Param(
+  [string]$serverModuleName
+)
+. .\shared.ps1
+$startFilePath="node_modules\$serverModuleName\$serverModuleName.start.js"
+$startFilePath= Convert-Path $startFilePath
+node $startFilePath
 $LASTEXITCODE=$true
--- stop.ps1
+++ stop.ps1
@@ -1,16 +1,8 @@
-$package= Load-NodePackage
-$moduleName=$package.name
-
-$dependencies=Get-ObjectProperties $package.dependencies
-
-Write-Host $dependencies
-
-# $stopFilePath="node_modules\$moduleName\$moduleName.stop.js"
-# $stopFilePath= Convert-Path $stopFilePath
-# [bool]$exists=(Test-Path $stopFilePath)
-# if ($exists -eq $false){
-#     $stopFilePath="$moduleName.stop.js"
-#     $stopFilePath= Convert-Path $startFilePath
-# }
-# node $stopFilePath
+Param(
+  [string]$serverModuleName
+)
+. .\shared.ps1
+$stopFilePath="node_modules\$serverModuleName\$serverModuleName.stop.js"
+$stopFilePath= Convert-Path $stopFilePath
+node $stopFilePath
 $LASTEXITCODE=$true

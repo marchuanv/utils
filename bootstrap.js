562f01f3e580fb3a5284fa82184bf6aeaf054cae
marchuanv <marchuanv@gmail.com>
2018-05-21 22:27:58 +0200

    automated commit

--- bootstrap.js
+++ bootstrap.js
@@ -1,2 +1,12 @@
-module.exports={ utils : require("./lib/utils.js"),}
+var section = process.argv.slice(2);
+console.log("");
+if (section=="start"){
+	console.log("STARTING");
+	
+}else if (section=="stop"){
+	console.log("STOPPING");
+	
+}else{
+	module.exports={ utils : require("./lib/utils.js"),}
+}
 

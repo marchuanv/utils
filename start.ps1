5127130ddfcc843d2df419ca065c23c2261ab081
Marchuan Van Der Merwe <marchuanv@sahomeloans.com>
2018-05-21 15:42:40 +0200

    automated commit

--- start.ps1
+++ start.ps1
@@ -2,4 +2,9 @@ $package= Load-NodePackage
 $moduleName=$package.name
 $startFilePath="node_modules\$moduleName\$moduleName.start.js"
 $startFilePath= Convert-Path $startFilePath
+[bool]$exists=(Test-Path $startFilePath)
+if ($exists -eq $false){
+    $startFilePath="$moduleName.start.js"
+    $startFilePath= Convert-Path $startFilePath
+}
 node $startFilePath
--- stop.ps1
+++ stop.ps1
@@ -2,4 +2,9 @@ $package= Load-NodePackage
 $moduleName=$package.name
 $stopFilePath="node_modules\$moduleName\$moduleName.stop.js"
 $stopFilePath= Convert-Path $stopFilePath
+[bool]$exists=(Test-Path $stopFilePath)
+if ($exists -eq $false){
+    $stopFilePath="$moduleName.stop.js"
+    $stopFilePath= Convert-Path $startFilePath
+}
 node $stopFilePath

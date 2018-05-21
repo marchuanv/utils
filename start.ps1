3733ede40ef2d5146ff64e6a9d397395da07343c
Marchuan Van Der Merwe <marchuanv@sahomeloans.com>
2018-05-21 15:35:07 +0200

    automated commit

--- start.ps1
+++ start.ps1
@@ -1,6 +1,5 @@
 $package= Load-NodePackage
 $moduleName=$package.name
-& .\stop.ps1
 $startFilePath="node_modules\$moduleName\$moduleName.start.js"
 $startFilePath= Convert-Path $startFilePath
 node $startFilePath

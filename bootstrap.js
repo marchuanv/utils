4cb293228c37b83f2a1a6a7378d1c8f09923a2b2
Marchuan Van Der Merwe <marchuanv@sahomeloans.com>
2018-05-22 15:09:49 +0200

    automated commit

--- bootstrap.js
+++ bootstrap.js
@@ -5,24 +5,67 @@ const fs = require('fs');
 const path = require('path');
 const package=require(path.join(__dirname, 'package.json'));
 const vm = require('vm');
-const bootstrapConfig=module.require('./bootstrap.json');
 const appFilePath=path.join(__dirname,"./lib/app.js");
+const shelljs=require("shelljs");
 
-modules["utils"]=[]; 
- modules["utils"].push("./lib/utils.js"); 
- modules["utils"].push("eventpublisher"); 
- modules["utils"].push("factory"); 
- modules["utils"].push("communication");
+const section = process.argv.slice(2);
 
-if (fs.existsSync(appFilePath)) {
-	const section = process.argv.slice(2);
-	cosnt app=require(appFilePath);
-	if (section=="start"){
-		app.start();
-	}else if (section=="stop"){
-		app.stop();
+
+//LOAD ALL THE MODULES THAT ARE NOT HARD REFERENCED
+package.submodules.forEach(function(mod){
+	if (mod.name==package.name) {
+		const softRefDependantModules=package.submodules.filter(function(mod2) { 
+			const matches=mod.modules.filter(function(m) { 
+				return mod2.name==m.name && m.ishardreference==false; 
+			});
+			if (matches.length > 0){
+				return true; 
+			}
+			return false;
+		});
+		softRefDependantModules.forEach(function(depModule) {
+
+		});
 	}
-}else{
+});
+
+modules["factory"].forEach(function(entry){
+	if (entry.scriptfilepath){
+		entry.scriptfilepath=path.resolve(entry.scriptfilepath);
+		var functions=new Object();
+		vm.createContext(functions);
+		var javascript=fs.readFileSync(entry.scriptfilepath,"utf8");
+		vm.runInNewContext(javascript, functions);
+		var scriptText=`module.exports={`;
+		for(const prop in functions) {
+			const func=functions[prop];
+			if (typeof func==='function'){
+				const funcName=utils.getFunctionName(func);
+				var funcStr=func.toString().replace(funcName,"");
+				funcStr=`${funcName}:${funcStr},`;
+				scriptText=`${scriptText}\r\n${funcStr}`;
+			}
+		};
+		scriptText= scriptText.replace(/(^,)|(,$)/g, "");
+		scriptText=`${scriptText}}`;
+		entry.script = scriptText;
+	}
+});
+
+const found = modules["factory"].forEach(function(entry){
+	if (entry.name == "app"){
+		console.log("app module info: ",entry);
+		const app=require(entry.scriptfilepath);
+		if (section=="start"){
+			app.start();
+		}else if (section=="stop"){
+			app.stop();
+		}
+		return true;
+	}
+});
+if (!found) {
 	module.exports=modules;
 }
+
 

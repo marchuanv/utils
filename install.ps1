96f21053155c70d129692b63db7b29f4293b4c41
Marchuan Van Der Merwe <marchuanv@sahomeloans.com>
2018-05-22 15:25:12 +0200

    automated commit

--- install.ps1
+++ install.ps1
@@ -7,4 +7,12 @@ npm install .
 cls
 Write-Host "UPDATING $moduleName"
 npm update .
+
+. .\shared.ps1
+$subModuleNames=$package.softDependencies | Select-Object -Property name
+foreach($subModuleName in $subModuleNames){
+    Remove-Submodules $subModuleName  
+    Add-Submodules $subModuleName
+}
+
 $LASTEXITCODE=$true

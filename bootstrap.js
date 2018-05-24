7121eb5528c330e65b315c5e9a6fad4b2f102f4d
Marchuan Van Der Merwe <marchuanv@sahomeloans.com>
2018-05-24 09:08:06 +0200

    automated commit

--- bootstrap.js
+++ bootstrap.js
@@ -5,16 +5,39 @@ const fs = require('fs');
 const path = require('path');
 const package=require(path.join(__dirname, 'package.json'));
 const vm = require('vm');
+const compress=require('node-minify');
 const appFilePath=path.join(__dirname,"./lib/app.js");
 const section = process.argv.slice(2);
-const modules=[];
-
-//LOAD ALL THE MODULES THAT ARE NOT HARD REFERENCED
-package.submodules.forEach(function(dep){
-	for(var propName in dep){
-		const submodulePath=path.join(__dirname, propName);
-		console.log("Reading submodule code files at ",submodulePath);
-	};
+const libraries=[];
+const librariesPath=path.join(__dirname,"lib");
+const files=fs.readdirSync(librariesPath).sort();
+const moduleLibrary=path.join(__dirname, `${package.name.min.js}`);
+files.forEach(fileName => {
+	const fullPath=path.join(__dirname, 'lib', fileName);
+	libraries.push(fullPath);
 });
-module.exports=modules;
+compress.minify({
+	compressor: 'no-compress',
+	input: libraries,
+	output: moduleLibrary,
+	callback: function (err) {
+		if (err){
+			var stack = new Error().stack
+			console.error(err);
+			console.log(stack);	
+		}
+		console.log(`${moduleLibrary} created.`);
+		const modules=new Object();
+		vm.createContext(modules);
+		var javascript=fs.readFileSync(moduleLibrary, "utf8");
+		vm.runInNewContext(javascript, modules);
+		package.submodules.forEach(function(dep){
+			const submoduleName=dep["name"];
+			const submoduleBootstrapPath=path.join(__dirname, submoduleName, "bootstrap.js");
+			const submoduleBootstrap = require(submoduleBootstrapPath);
+			modules[submoduleName]=submoduleBootstrap
+		});
+		module.exports=modules;
+	}
+});
 

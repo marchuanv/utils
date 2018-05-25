8e2dac5bf7126a99640436b5f5d0f453e87c8146
Marchuan Van Der Merwe <marchuanv@sahomeloans.com>
2018-05-25 15:14:33 +0200

    automated commit

--- bootstrap.js
+++ bootstrap.js
@@ -7,7 +7,7 @@ const package=require(path.join(__dirname, 'package.json'));
 const vm = require('vm');
 const compress=require('node-minify');
 const appFilePath=path.join(__dirname,"./lib/app.js");
-const section = process.argv.slice(2);
+const startStop = process.argv.slice(2);
 const libraries=[];
 const librariesPath=path.join(__dirname,"lib");
 const files=fs.readdirSync(librariesPath).sort();
@@ -21,22 +21,39 @@ compress.minify({
 	input: libraries,
 	output: moduleLibrary,
 	callback: function (err) {
+		
 		if (err){
 			var stack = new Error().stack
 			console.error(err);
 			console.log(stack);	
+			return;
 		}
+
 		console.log(`${moduleLibrary} created.`);
-		const modules=new Object();
+		const modules={
+		  require: require,
+		  console: console,
+		  package: package
+		};
+		
 		vm.createContext(modules);
+		
 		var javascript=fs.readFileSync(moduleLibrary, "utf8");
-		vm.runInNewContext(javascript, modules);
+		var script = new vm.Script(javascript);
+		script.runInNewContext(modules);
+
 		package.submodules.forEach(function(dep){
 			const submoduleName=dep["name"];
 			const submoduleBootstrapPath=path.join(__dirname, submoduleName, "bootstrap.js");
-			const submoduleBootstrap = require(submoduleBootstrapPath);
-			modules[submoduleName]=submoduleBootstrap
+			const bootstrapSubmodule = require(submoduleBootstrapPath);
+			modules[submoduleName]=bootstrapSubmodule
+			console.log(bootstrapSubmodule);
 		});
+
 		module.exports=modules;
+		if (modules.App){
+			const app=new modules.App();
+			app[startStop]();
+		}
 	}
 });

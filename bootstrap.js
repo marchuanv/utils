e7a564790c6f86244dbf3845944e279de0117bf4
Marchuan Van Der Merwe <marchuanv@sahomeloans.com>
2018-05-25 16:45:18 +0200

    automated commit

--- bootstrap.js
+++ bootstrap.js
@@ -1,5 +1,3 @@
-console.log("");
-
 const os = require('os');
 const fs = require('fs');
 const path = require('path');
@@ -13,39 +11,35 @@ const appFilePath=path.join(__dirname,"./lib/app.js");
 const startStop = process.argv.slice(2);
 const libraries=[];
 const librariesPath=path.join(__dirname,"lib");
+
 const files=fs.readdirSync(librariesPath).sort();
-const moduleLibrary=path.join(__dirname, `${package.name}.min.js`);
 files.forEach(fileName => {
 	const fullPath=path.join(__dirname, 'lib', fileName);
 	libraries.push(fullPath);
 });
 
+const moduleLibrary=path.join(__dirname, `${package.name}.min.js`);
 compress.minify({
 	compressor: 'no-compress',
 	input: libraries,
 	output: moduleLibrary,
 	callback: function (err) {
-		
 		if (err){
 			var stack = new Error().stack
 			console.error(err);
 			console.log(stack);	
 			return;
 		}
-
 		console.log(`${moduleLibrary} created.`);
 		const modules={
 		  require: require,
 		  console: console,
 		  package: package
 		};
-		
 		vm.createContext(modules);
-		
 		var javascript=fs.readFileSync(moduleLibrary, "utf8");
 		var script = new vm.Script(javascript);
 		script.runInNewContext(modules);
-
 		package.submodules.forEach(function(dep){
 			const submoduleName=dep["name"];
 			if (isWindows==true) {
@@ -72,7 +66,6 @@ compress.minify({
 			const bootstrapSubmodule = require(submoduleBootstrapPath);
 			modules[submoduleName]=bootstrapSubmodule
 		});
-
 		for(var propName in package.dependencies){
 			if (isWindows==true) {
 			  let shell = new Powershell({
@@ -93,9 +86,7 @@ compress.minify({
 			var friendlyPropName=propName.replace("-","").replace(".","").replace(" ","");
 			modules[friendlyPropName]=require(propName);	
 		}
-
 		module.exports=modules;
-		
 		if (modules.App){
 			const app=new modules.App();
 			app[startStop]();

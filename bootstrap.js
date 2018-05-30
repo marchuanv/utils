15cf50b3ab691a78d9e8511ed729c838e38682cf
Marchuan Van Der Merwe <marchuanv@sahomeloans.com>
2018-05-30 14:02:52 +0200

    automated commit

--- bootstrap.js
+++ bootstrap.js
@@ -15,6 +15,9 @@ const libraries=[];
 const librariesPath=path.join(__dirname,"lib");
 const files=fs.readdirSync(librariesPath).sort();
 const moduleLibrary=path.join(__dirname, `${package.name}.min.js`);
+const port=process.env.PORT;
+const host= process.env.IP || os.hostname();
+
 files.forEach(fileName => {
 	const fullPath=path.join(__dirname, 'lib', fileName);
 	libraries.push(fullPath);
@@ -24,6 +27,16 @@ const modules={
   require: require,
   console: console,
   package: package
+};
+
+function newGuid(){
+    var d = new Date().getTime();
+    var uuid = 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g, function(c) {
+        var r = (d + Math.random()*16)%16 | 0;
+        d = Math.floor(d/16);
+        return (c=='x' ? r : (r&0x3|0x8)).toString(16);
+    });
+    return uuid;
 };
 
 compress.minify({
@@ -44,13 +57,22 @@ compress.minify({
 		var script = new vm.Script(javascript);
 		script.runInNewContext(modules);
 
+		var dependencies=[];
 		for(var propName in package.dependencies){
+			var friendlyPropName=propName.replace("-","").replace(".","").replace(" ","");
+			dependencies.push({
+				name: propName,
+				friendlyname: friendlyPropName
+			});
+		};
+
+		for(var dep in dependencies){
 			if (isWindows==true) {
 			  let shell = new Powershell({
 			    executionPolicy: 'Bypass',
 			    noProfile: true
 			  });
-			  shell.addCommand(`npm update ${propName}`);
+			  shell.addCommand(`npm update ${dep.name}`);
 			  shell.invoke().then(output => {
 			    console.log(output);
 			    shell.dispose();
@@ -59,40 +81,96 @@ compress.minify({
 			    shell.dispose();
 			  });
 			}else{
-				shell.exec(`npm update ${propName}`);
+				shell.exec(`npm update ${dep.name}`);
 			}
-			var friendlyPropName=propName.replace("-","").replace(".","").replace(" ","");
-			modules[friendlyPropName]=require(propName);	
-		}
+			modules[dep.friendlyname]=require(dep.name);	
+		};
 
-		for(var submodule in package.submodules){
-			if (isWindows==true) {
-			  let shell = new Powershell({
-			    executionPolicy: 'Bypass',
-			    noProfile: true
-			  });
-			  shell.addCommand(`npm update ${propName}`);
-			  shell.invoke().then(output => {
-			    console.log(output);
-			    shell.dispose();
-			  }).catch(err => {
-			    console.log(err);
-			    shell.dispose();
-			  });
-			}else{
-				shell.exec(`npm update ${propName}`);
+		if (package.name != "communication"){
+			var communication=require('communication');
+			const factorycommunication= new communication.WebSocket(package.factoryhost, package.factoryport);
+			const eventpublishercommunication= new communication.WebSocket(package.eventhost, package.eventport);
+			const client= new communication.WebSocket(package.host, package.port);
+			
+			const factoryChannelName="factory";
+			const getInstanceEventName="getinstance";
+			const registerTypesEventName="registerclass";
+			const factoryInstruction= {ref: null, class: null, instance: null};
+			
+			if (package.factoryhost==host && package.factoryport==port) {
+				const factory = new modules.Factory();
+				factorycommunication.receive(function response(instruction, cbRespond){
+					if (instruction.ref){
+						factory.getInstance(instruction.ref, function(instance){
+							instruction.instance=instance;
+							cbRespond(instruction);
+						});
+					}else if (instruction.class){
+						factory.registerFunction(instruction.class, function(ref){
+							instruction.ref=ref;
+							cbRespond(instruction);
+						});
+					}else{
+						cbRespond(null);
+					}
+				});
+			} else if (package.eventhost==host && package.eventport==port) {
+				eventpublishercommunication.receive(function response(event){
+					if (event.channel==factoryChannelName && event.name == getInstanceEventName){
+
+						factoryInstruction.ref=event.data.ref;
+						factoryInstruction.class = null;
+						factoryInstruction.instance = null;
+
+						factorycommunication.send(factoryInstruction, function received(res) {
+							if (res.instance){
+								const objFoundEvent={ name:"objectfound", channel: factoryChannelName, data: res.instance };
+								eventpublishercommunication.send(objFoundEvent, function received() {
+								},function(err){
+									throw "failed to send event, infrastructure is not working"
+								});
+							}else{
+								const objNotFoundEvent={ name:"objectnotfound", channel: factoryChannelName, data: {} };
+								eventpublishercommunication.send(objNotFoundEvent, function received() {
+								},function(err){
+									throw "failed to send event, infrastructure is not working"
+								});
+							}
+						},function failed(){
+							throw "failed to send event, infrastructure is not working"
+						});
+					}
+					if (event.channel==factoryChannelName && event.name == registerTypesEventName){
+						
+						factoryInstruction.ref = null;
+						factoryInstruction.class=event.data.function;
+						factoryInstruction.instance = null;
+
+						factorycommunication.send(factoryInstruction, function received(res) {
+							if (res.ref){
+								const registeredEvent={ name:"registered", channel: factoryChannelName, data: {} };
+								eventpublishercommunication.send(registeredEvent, function received() {
+								},function(){
+									throw "failed to send event, infrastructure is not working"
+								});
+							}else{
+								const notRegisteredEvent={ name:"notregistered", channel: factoryChannelName, data: {} };
+								eventpublishercommunication.send(notRegisteredEvent, function received() {
+								},function(){
+									throw "failed to send event, infrastructure is not working"
+								});
+							}
+						},function failed(){
+							throw "failed to send event, infrastructure is not working"
+						});
+					}
+				});
+			} else {
+				var event={ name:"initialise${}", data:  };
+				eventpublishercommunication.send(message, function received(remoteMessage) {
+				},function failed(){
+				});
 			}
-			var friendlyPropName=propName.replace("-","").replace(".","").replace(" ","");
-			modules[friendlyPropName]=require(propName);	
-		}
-
-		if (modules.Server){
-			const server=new modules.Server();
-			server.start();
-		}
-		if (modules.Client){
-			const client=new modules.Client();
-			client.start();
 		}
 	}
 });

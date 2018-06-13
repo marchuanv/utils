47f0aec629c060c20659bae4334b4aa307a7427a
marchuanv <marchuanv@gmail.com>
2018-06-13 04:17:13 +0200

    automated commit

--- bootstrap.js
+++ bootstrap.js
@@ -11,7 +11,6 @@ const Powershell=require('node-powershell');
 const compress=require('node-minify');
 const appFilePath=path.join(__dirname,"./lib/app.js");
 const startStop = process.argv.slice(2);
-const libraries=[];
 const librariesPath=path.join(__dirname,"lib");
 const bootstrapExtPath=path.join(__dirname,"bootstrap.ext.js");
 const files=fs.readdirSync(librariesPath).sort();
@@ -19,12 +18,30 @@ const moduleLibrary=path.join(__dirname, `${package.name}.min.js`);
 const port=process.env.PORT;
 const host= process.env.IP || os.hostname();
 
+const libraries=[];
 files.forEach(fileName => {
 	const fullPath=path.join(__dirname, 'lib', fileName);
 	libraries.push(fullPath);
 });
 
-const modules=new Object();
+for(var propName in package.dependencies){
+	if (isWindows==true) {
+	  let shell = new Powershell({
+	    executionPolicy: 'Bypass',
+	    noProfile: true
+	  });
+	  shell.addCommand(`npm update ${propName}`);
+	  shell.invoke().then(output => {
+	    console.log(output);
+	    shell.dispose();
+	  }).catch(err => {
+	    console.log(err);
+	    shell.dispose();
+	  });
+	}else{
+		shell.exec(`npm update ${propName}`);
+	}
+};
 
 compress.minify({
 	compressor: 'no-compress',
@@ -37,62 +54,18 @@ compress.minify({
 			console.log(stack);	
 			return;
 		}
+		process.mainModule.libraries={};
 		console.log(`${moduleLibrary} created.`);
-		vm.createContext(modules);
+		vm.createContext(process.mainModule.libraries);
 		var javascript=fs.readFileSync(moduleLibrary, "utf8");
 		var script = new vm.Script(javascript);
-		script.runInNewContext(modules);
+		script.runInNewContext(process.mainModule.libraries);
 	}
 });
 
-var dependencies=[];
-for(var propName in package.dependencies){
-	var friendlyPropName=propName.replace("-","").replace(".","").replace(" ","");
-	dependencies.push({
-		name: propName,
-		friendlyname: friendlyPropName
-	});
-};
-
-dependencies.forEach(function(dep){
-	if (isWindows==true) {
-	  let shell = new Powershell({
-	    executionPolicy: 'Bypass',
-	    noProfile: true
-	  });
-	  shell.addCommand(`npm update ${dep.name}`);
-	  shell.invoke().then(output => {
-	    console.log(output);
-	    shell.dispose();
-	  }).catch(err => {
-	    console.log(err);
-	    shell.dispose();
-	  });
-	}else{
-		shell.exec(`npm update ${dep.name}`);
-	}
-	const friendlyname=dep.friendlyname;
-	const name=dep.name;
-	const depModules=require(name);
-	for(var propName in depModules){
-		modules[propName]=depModules[propName];	
-	};
-});
-
-setTimeout(function(){
-	console.log("");
-	console.log(`-------------------------------< LOADED ALL MODULES FOR ${package.name} >-----------------------------------`);
-	dependencies.forEach(function(dep){
-		console.log(dep);
-	});
-	console.log(`------------------------------------------------------------------------------------------------------------`);
-	console.log("");
-	if (fs.existsSync(bootstrapExtPath)) {
-		console.log("loading ", bootstrapExtPath);
-	  	const modulesExt=require(bootstrapExtPath);
-	  	for(const propname in modulesExt){
-	  		modules[propname]=modulesExt[propname]
-	  	};
-	}
-},5000);
-module.exports=modules;
+if (fs.existsSync(bootstrapExtPath)) {
+	console.log("loading ", bootstrapExtPath);
+  	module.exports=require(bootstrapExtPath);
+}else{
+	module.exports={};
+}

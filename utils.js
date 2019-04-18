4b72e6c654d5a8f4928aecb1f3bae9f6e141685f
marchuanv <marchuanv@gmail.com>
2019-04-18 04:07:50 +0200

    fix

--- utils.js
+++ utils.js
@@ -21,9 +21,17 @@ function Utils(){
          return Buffer.byteLength(str, 'utf8');
     }
     
-    this.syncObject=function(obj, sourceObj, replace){
+    this.syncObject=function(obj, sourceObj){
         for(const propName in obj){
-            if (sourceObj[propName] || replace === true) {
+            if (sourceObj[propName]) {
+                obj[propName] = sourceObj[propName];
+            }
+        };
+    };
+
+    this.extendObject=function(obj, sourceObj){
+        for(const propName in sourceObj){
+            if (sourceObj[propName]) {
                 obj[propName] = sourceObj[propName];
             }
         };

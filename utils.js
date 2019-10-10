d667a83c7cdae071743bf08eeb66fc383a09edfc
marchuanv <Marchuanv@gmail.com>
2019-10-10 11:03:52 +0200

    Update utils.js

--- utils.js
+++ utils.js
@@ -9,6 +9,36 @@ function Utils({ fs, vm, path }){
           return Object.keys(obj)[0];
     };
 
+    this.sizeOf = (obj) => {
+        let bytes=0;
+        switch(typeof obj) {
+            case 'number':
+                bytes += 8;
+                break;
+            case 'string':
+                bytes += obj.length * 2;
+                break;
+            case 'boolean':
+                bytes += 4;
+                break;
+            case 'object':
+                var objClass = Object.prototype.toString.call(obj).slice(8, -1);
+                if(objClass === 'Object' || objClass === 'Array') {
+                    for(var key in obj) {
+                        if(obj.hasOwnProperty(key)){
+                            const prop = obj[key];
+                            bytes = bytes + this.sizeOf(prop);
+                        } else {
+                            continue
+                        }
+                    }
+                } else {
+                    bytes += obj.toString().length * 2
+                };
+                break;
+        }
+        return bytes;
+    };
     
     this.toArrayBuffer=function(str) {
         var ab = new ArrayBuffer(str.length);

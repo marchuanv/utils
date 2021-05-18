9ebeb551f1d9d92b440d535e47a10282f344e935
marchuanv <marchuanv@gmail.com>
2021-05-18 12:16:26 +0200

    fix

--- utils.js
+++ utils.js
@@ -196,7 +196,8 @@ function Utils({ fs, vm, crypto, fsPath }){
     this.getJSONObject=function(jsonString, includeFunctions=false){
       try{
           const dateFormat = /^(-?(?:[1-9][0-9]*)?[0-9]{4})-(1[0-2]|0[1-9])-(3[01]|0[1-9]|[12][0-9])T(2[0-3]|[01][0-9]):([0-5][0-9]):([0-5][0-9])(.[0-9]+)?(Z)?$/;
-          return JSON.parse(jsonString, (key, value) => {
+          jsonString = jsonString.replace(/\\n/g, '');
+          const obj = JSON.parse(jsonString, (key, value) => {
             if (typeof value === "string" && dateFormat.test(value)) {
               return new Date(value);
             }
@@ -206,7 +207,7 @@ function Utils({ fs, vm, crypto, fsPath }){
             }
             return value;
           });
-
+          return obj;
       }catch(err){
         return null;
       }

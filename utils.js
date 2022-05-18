e4fc81e3a2c5cd1694dbc339cee9af5f257a7a78
marchuanv <Marchuanv@gmail.com>
2022-05-18 14:23:29 +0200

    Update utils.js

--- utils.js
+++ utils.js
@@ -113,12 +113,7 @@ function Utils({ fs, vm, crypto, fsPath }){
     };
     
     this.generateGUID=function() {
-       function uuidv4() {
-          return ([1e7]+-1e3+-4e3+-8e3+-1e11).replace(/[018]/g, c =>
-            (c ^ crypto.getRandomValues(new Uint8Array(1))[0] & 15 >> c / 4).toString(16)
-          );
-        }
-        return uuidv4();
+      return crypto.randomUUID();
     };
     
     this.getFunctionName=function(func) {

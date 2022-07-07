a7908750b7a0b031dd4dc17738608c322ad34216
marchuanv <Marchuanv@gmail.com>
2022-07-07 09:28:54 +0200

    Update utils.js

--- utils.js
+++ utils.js
@@ -1,7 +1,7 @@
 function Utils({ fs, vm, crypto, fsPath }){
 
   const whiteSpaceRegEx = new RegExp(/\s*/,"g");
-  const funcDestructionMatch = new RegExp(/\s*function\s*[A-z]+\s*\(\s*\{\s*((?:\s*[A-z0-9]+\s*\,)+)(\s*[A-z0-9]+\s*)\}\s*\)/);
+  const funcDestructionMatch = new RegExp(/\s*function\s*[A-z]+\s*\(\s*\{\s*(((?:\s*[A-z0-9]+\s*\,)+)(\s*[A-z0-9]+\s*)|(\s*[A-z0-9]+\s*)|\s*)\s*\}\s*\)/);
   const funcParamMatch = new RegExp(/\s*function\s*[A-z]+\s*\((((?:\s*[A-z0-9]+\s*\,)+)(\s*[A-z0-9]+\s*)|(\s*[A-z0-9]+\s*)|\s*)\)/);
   const classCtorParamMatch = new RegExp(/constructor\s*\((\s*[A-z0-9,]\s*)+\)\s*\{/);
   const base64RegEx = new RegExp(/^([A-Za-z0-9+/]{4})*([A-Za-z0-9+/]{3}=|[A-Za-z0-9+/]{2}==)?$/,"g");

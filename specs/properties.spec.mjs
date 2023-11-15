c511f6d0dcdd1009e18c24d27baa5755959b608c
Marchuan Van Der Merwe <marchuanv@sahomeloans.com>
2023-11-15 18:27:14 +0200

    refactor

--- specs/properties.spec.mjs
+++ specs/properties.spec.mjs
@@ -13,7 +13,7 @@ const suite = describe('when properties change', () => {
         const actualValue = 'ef7cbdeb-6536-4e38-a9e1-cc1acdd00e7d';
         properties.Id = actualValue;
         expect(properties.Id).toBe(actualValue);
-        properties.onChange('Id', (value) => {
+        properties.onSet('Id', (value) => {
             return actualValue;
         });
         const expectedValue = '5a0bf50b-6ba5-4570-984c-cdcada1d19f0';
@@ -25,7 +25,7 @@ const suite = describe('when properties change', () => {
         const actualValue = 'b4ad82a4-d76e-4b36-bc4d-f0ca5386622f';
         properties.Id = actualValue;
         expect(properties.Id).toBe(actualValue);
-        properties.onChange('Id', (value) => {
+        properties.onSet('Id', (value) => {
             return actualValue;
         });
         const expectedValue = '1deccbb7-b859-4e77-a1b5-7b5b58ce3b31';

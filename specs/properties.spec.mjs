42b7bc93a7a56a372aaab63ec4f085d653a40ab7
Marchuan Van Der Merwe <marchuanv@sahomeloans.com>
2023-11-15 17:11:57 +0200

    refactor

--- specs/properties.spec.mjs
+++ specs/properties.spec.mjs
@@ -1,7 +1,15 @@
 import { Properties } from '../lib/registry.mjs';
+class PropertiesTest extends Properties {
+    get Id() {
+        return super.get('Id');
+    }
+    set Id(value) {
+        super.set('Id', value)
+    }
+}
 const suite = describe('when properties change', () => {
     it('should sync data', () => {
-        const properties = new Properties();
+        const properties = new PropertiesTest();
         const actualValue = 'ef7cbdeb-6536-4e38-a9e1-cc1acdd00e7d';
         properties.Id = actualValue;
         expect(properties.Id).toBe(actualValue);
@@ -13,7 +21,7 @@ const suite = describe('when properties change', () => {
         expect(properties.Id).toBe(actualValue);
     });
     it('should sync data within context', () => {
-        const properties = new Properties();
+        const properties = new PropertiesTest();
         const actualValue = 'b4ad82a4-d76e-4b36-bc4d-f0ca5386622f';
         properties.Id = actualValue;
         expect(properties.Id).toBe(actualValue);

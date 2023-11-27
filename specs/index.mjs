cbf885a8d3b20765034c514af864b3410fa4a054
Marchuan Van Der Merwe <marchuanv@sahomeloans.com>
2023-11-28 00:57:38 +0200

    refactor

---
+++ specs/index.mjs
@@ -0,0 +1,3 @@
+import { Specs } from '../registry.mjs';
+const specs = new Specs(10000, './');
+specs.run();
---
+++ specs/reflection.spec.mjs
@@ -0,0 +1,41 @@
+
+import { reflection } from '../registry.mjs';
+describe('when ', () => {
+    it('should', () => {
+        const member = reflection.getMember(Baby, Human);
+        expect(member).toBeDefined();
+
+        const humanMember = member.find(Human.name, true, false, false);
+        expect(humanMember).toBeDefined();
+
+        const babyProperties = member.findAll('name', false, true, false);
+        expect(babyProperties.length).toBe(2);
+
+        const humanProperties = humanMember.findAll('age', false, true, false);
+        expect(humanProperties.length).toBe(2);
+    });
+});
+
+class Human {
+    constructor() {
+        this._age = 0;
+    }
+    get age() {
+        return this._age;
+    }
+    set age(value) {
+        this._age = value;
+    }
+}
+
+class Baby extends Human {
+    constructor(name) {
+        this._name = name;
+    }
+    get name() {
+        return this._name;
+    }
+    set name(value) {
+        this._name = value;
+    }
+}

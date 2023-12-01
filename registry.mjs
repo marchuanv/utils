a9e42142b1bb067cc25920b4b20530f2b963ccae
Marchuan Van Der Merwe <marchuanv@sahomeloans.com>
2023-12-01 16:46:36 +0200

    refactor

--- registry.mjs
+++ registry.mjs
@@ -3,6 +3,7 @@ import Jasmine from 'jasmine';
 import { fileURLToPath, pathToFileURL } from 'url';
 import { General } from './lib/general.mjs';
 import { Import } from './lib/import.mjs';
+import { Member } from './lib/member.mjs';
 import { Reflection } from './lib/reflection.mjs';
 import { Security } from './lib/security.mjs';
 import { Specs } from './lib/specs.mjs';
@@ -13,16 +14,18 @@ const security = new Security();
 const reflection = new Reflection();
 const { walkDir } = general;
 
-export { Member } from './lib/member.mjs';
 export { EventEmitter } from 'events';
 export { Buffer } from 'node:buffer';
 export { constants, createHmac, generateKeyPairSync, privateDecrypt, publicEncrypt, randomBytes, randomUUID } from 'node:crypto';
 export { existsSync, lstatSync, readFileSync, readdirSync, statSync } from 'node:fs';
 export { basename, join, resolve } from 'node:path';
-export { Member } from './lib/member.mjs';
+export { ClassMember } from './lib/class.member.mjs';
+export { MemberParameter } from './lib/member.parameter.mjs';
+export { MethodMember } from './lib/method.member.mjs';
+export { PropertyMember } from './lib/property.member.mjs';
 
 const baseUrl = import.meta.url;
 const currentDir = fileURLToPath(new URL('./', baseUrl));
 
-export { Jasmine, Specs, addSchema, currentDir, fileURLToPath, general, importExtended, pathToFileURL, reflection, security, validate, walkDir };
+export { Jasmine, Member, Specs, addSchema, currentDir, fileURLToPath, general, importExtended, pathToFileURL, reflection, security, validate, walkDir };
 
--- specs/reflection.spec.mjs
+++ specs/reflection.spec.mjs
@@ -1,50 +1,62 @@
+import { ClassMember } from "../lib/class.member.mjs";
 
-import { reflection } from '../registry.mjs';
 describe('when ', () => {
     it('should', () => {
-        const member = reflection.getMember(Baby, Human);
-        expect(member).toBeDefined();
-
-        const humanMember = member.find(Human.name, true, false, false);
-        expect(humanMember).toBeDefined();
-
-        const babyProperties = member.findAll('name', false, true, false);
-        expect(babyProperties.length).toBe(2);
-
-        const humanProperties = humanMember.findAll('age', false, true, false);
-        expect(humanProperties.length).toBe(2);
+        const classMember = new ClassMember(Baby);
     });
 });
 
 class Human {
+    /**
+     * @param { Number } age
+     * @param { Array<String> } parts
+     * @param { Number } height
+     * @param { Number } weight
+     * @param {{ heart: Boolean }} organs
+    */
     constructor(age = 1, parts = ['head', 'feet', 'legs', 'arms'], height, weight, organs = { heart: true }) {
         this._age = age;
     }
+    /**
+     * @returns { Number }
+    */
     get age() {
         return this._age;
     }
+    /**
+     * @param { Number } value
+    */
     set age(value) {
         this._age = value;
+    }
+    /**
+     * @param { Number } age
+     * @param { Array<String> } parts
+     * @param { Number } height
+     * @param { Number } weight
+     * @param {{ heart: Boolean }} organs
+    */
+    static create(age = 1, parts = ['head', 'feet', 'legs', 'arms'], height, weight, organs = { heart: true }) {
     }
 }
 
 class Baby extends Human {
+    /**
+     * @param { String } name
+    */
     constructor(name) {
         this._name = name;
     }
+    /**
+     * @returns { String }
+    */
     get name() {
         return this._name;
     }
+    /**
+     * @param { String } value
+    */
     set name(value) {
         this._name = value;
-    }
-    static ctor() {
-        
-    }
-    static get ctorProp() {
-        
-    }
-    static set ctorProp(value) {
-        
     }
 }

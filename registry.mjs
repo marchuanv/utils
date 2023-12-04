45c6c9f6ef03add4d193cc91223e8820e5050986
Marchuan Van Der Merwe <marchuanv@sahomeloans.com>
2023-12-04 10:08:48 +0200

    refactor

--- registry.mjs
+++ registry.mjs
@@ -3,7 +3,7 @@ import Jasmine from 'jasmine';
 import { fileURLToPath, pathToFileURL } from 'url';
 import { General } from './lib/general.mjs';
 import { Import } from './lib/import.mjs';
-import { Member } from './lib/member.mjs';
+import { Member } from './lib/reflect/member.mjs';
 import { Security } from './lib/security.mjs';
 import { Specs } from './lib/specs.mjs';
 
@@ -17,10 +17,10 @@ export { Buffer } from 'node:buffer';
 export { constants, createHmac, generateKeyPairSync, privateDecrypt, publicEncrypt, randomBytes, randomUUID } from 'node:crypto';
 export { existsSync, lstatSync, readFileSync, readdirSync, statSync } from 'node:fs';
 export { basename, join, resolve } from 'node:path';
-export { ClassMember } from './lib/class.member.mjs';
-export { MemberParameter } from './lib/member.parameter.mjs';
-export { MethodMember } from './lib/method.member.mjs';
-export { PropertyMember } from './lib/property.member.mjs';
+export { ClassMember } from './lib/reflect/class.member.mjs';
+export { MemberParameter } from './lib/reflect/member.parameter.mjs';
+export { MethodMember } from './lib/reflect/method.member.mjs';
+export { PropertyMember } from './lib/reflect/property.member.mjs';
 
 const baseUrl = import.meta.url;
 const currentDir = fileURLToPath(new URL('./', baseUrl));

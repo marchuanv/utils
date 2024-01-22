bb49a32924ec11f87e37728add4e9fb7dfa21e98
Marchuan Van Der Merwe <marchuanv@sahomeloans.com>
2024-01-22 22:31:09 +0200

    refactor

--- specs/index.mjs
+++ specs/index.mjs
@@ -3,9 +3,16 @@ import { Animal } from './classes/animal.mjs';
 import { Dog } from './classes/dog.mjs';
 import { Food } from './classes/food.mjs';
 const currentDir = fileURLToPath(new URL('./', import.meta.url));
-Container.register(join(currentDir, 'classes', 'animal.interface.json'), Animal);
-Container.register(join(currentDir, 'classes', 'food.interface.json'), Food);
-Container.register(join(currentDir, 'classes', 'dog.interface.json'), Dog);
+Container.validate(join(currentDir, 'classes', 'animal.interface.json'), Animal).then(() => {
+    Container.register(Animal);
+    Container.validate(join(currentDir, 'classes', 'food.interface.json'), Food).then(() => {
+        Container.register(Food);
+        Container.validate(join(currentDir, 'classes', 'dog.interface.json'), Dog).then(() => {
+            Container.register(Dog);
+            const specs = new Specs(60000, './');
+            specs.run();
+        });
+    });
+});
 export { Animal, Dog, Food };
-const specs = new Specs(60000, './');
-specs.run();
+

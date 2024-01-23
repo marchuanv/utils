3c4b6082b77b464ad9480100de2fdd07b6490855
Marchuan Van Der Merwe <marchuanv@sahomeloans.com>
2024-01-24 00:17:04 +0200

    cleanup

--- registry.mjs
+++ registry.mjs
@@ -3,15 +3,11 @@ import { VERBOSE } from "@hyperjump/json-schema/experimental";
 import Jasmine from 'jasmine';
 import { fileURLToPath, pathToFileURL } from 'url';
 import vm from "vm";
-
-import { Container } from './lib/container.mjs';
 import { walkDir } from './lib/general.mjs';
 import { GUID } from './lib/guid.mjs';
 import { Import } from './lib/import.mjs';
-import { Member } from './lib/member.mjs';
 import { Security } from './lib/security.mjs';
 import { Specs } from './lib/specs.mjs';
-import { TypeDefinition } from "./lib/type.definition.mjs";
 
 const importExtended = new Import();
 const security = new Security();
@@ -21,27 +17,19 @@ export { Buffer } from 'node:buffer';
 export { constants, createHmac, generateKeyPairSync, privateDecrypt, publicEncrypt, randomBytes } from 'node:crypto';
 export { existsSync, lstatSync, readFileSync, readdirSync, statSync } from 'node:fs';
 export { basename, dirname, extname, join, resolve } from 'node:path';
-export { ClassIntegrity } from './lib/class.integrity.mjs';
-export { ClassInterface } from './lib/class.interface.mjs';
-export { MemberParameter } from './lib/member.parameter.mjs';
-export { MethodMember } from './lib/method.member.mjs';
-export { PrimitiveType } from './lib/primitivetype.mjs';
-export { PropertyMember } from './lib/property.member.mjs';
-export { ReferenceType } from './lib/referencetype.mjs';
-export { Schema } from './lib/schema.mjs';
 
 setMetaSchemaOutputFormat(VERBOSE);
 
 export {
-    Container, GUID,
+    GUID,
     Jasmine,
-    Member,
     Specs,
-    TypeDefinition,
     VERBOSE,
-    addSchema, fileURLToPath,
+    addSchema,
+    fileURLToPath,
     importExtended,
-    pathToFileURL, security,
+    pathToFileURL,
+    security,
     validate as validateSchema,
     vm,
     walkDir
--- specs/index.mjs
+++ specs/index.mjs
@@ -1,18 +1,3 @@
-import { Specs, TypeDefinition, fileURLToPath, join } from "../registry.mjs";
-import { Animal } from "./classes/animal.mjs";
-import { Dog } from "./classes/dog.mjs";
-import { Food } from "./classes/food.mjs";
-let currentDir = fileURLToPath(new URL('./', import.meta.url));
-const scriptsDirPath = join(currentDir, 'classes');
-TypeDefinition.register([
-    { scriptsDirPath, scriptFileName: 'animal.mjs', targetClass: Animal },
-    { scriptsDirPath, scriptFileName: 'food.mjs', targetClass: Food },
-    { scriptsDirPath, scriptFileName: 'dog.mjs', targetClass: Dog },
-]).then(() => {
-    const specs = new Specs(60000, './');
-    specs.run();
-}).catch((error) => {
-    console.error(error);
-});
-export { Animal, Dog, Food };
-
+import { Specs } from "../registry.mjs";
+const specs = new Specs(60000, './');
+specs.run();

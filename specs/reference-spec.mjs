a5ef5b8dc0c2b1b217988f600d3e80d179953d91
marchuanv <marchuanv@gmail.com>
2024-02-15 22:54:55 +0200

    refactor

--- specs/reference-spec.mjs
+++ specs/reference-spec.mjs
@@ -1,10 +1,11 @@
-import { Reference } from '../registry.mjs';
+import { GUID, Reference } from '../registry.mjs';
 describe('when reference', () => {
     it('should', () => {
-        const { Id, refId } = new Reference();
-        const foundRefId = Reference.get(Id);
-        expect(foundRefId).toBeDefined();
-        expect(foundRefId).not.toBeNull();
-        expect(foundRefId).toBe(refId);
+        const refA = Reference.create();
+        const refB = Reference.create(refA.Id);
+        const ref = refB.find(({ Id }) => Id === refA.Id);
+        expect(ref).toBeDefined();
+        expect(ref).not.toBeNull();
+        expect(ref.Id).toBe(refA.Id);
     });
 });

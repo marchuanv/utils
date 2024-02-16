48b49dd869af20cd69d9f3a84a6c80dbd27d3718
Marchuan Van Der Merwe <marchuanv@sahomeloans.com>
2024-02-16 09:20:08 +0200

    more specs

--- specs/reference-spec.mjs
+++ specs/reference-spec.mjs
@@ -1,14 +1,11 @@
 import { Reference } from '../registry.mjs';
 describe('when finding references', () => {
-
     let A_Id = null;
     let B_Id = null;
     let C_Id = null;
-
     let A = null;
     let B = null;
     let C = null;
-
     beforeAll(() => {
         A = Reference.create();
         ({ Id: A_Id } = A);
@@ -21,16 +18,26 @@ describe('when finding references', () => {
             ({ Id: C_Id } = C);
         }
     });
-
     it('B should reference A', () => {
         const isRef = B.references(Id => Id === A_Id);
         expect(isRef).toBeDefined();
         expect(isRef).not.toBeNull();
         expect(isRef).toBeTrue();
     });
-
     it('A should reference B', () => {
         const isRef = A.references(Id => Id === B_Id);
+        expect(isRef).toBeDefined();
+        expect(isRef).not.toBeNull();
+        expect(isRef).toBeTrue();
+    });
+    it('B should reference C', () => {
+        const isRef = B.references(Id => Id === C_Id);
+        expect(isRef).toBeDefined();
+        expect(isRef).not.toBeNull();
+        expect(isRef).toBeTrue();
+    });
+    it('C should reference B', () => {
+        const isRef = C.references(Id => Id === B_Id);
         expect(isRef).toBeDefined();
         expect(isRef).not.toBeNull();
         expect(isRef).toBeTrue();

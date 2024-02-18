d59909519e254f600e7172f7b6bba157691ede78
marchuanv <marchuanv@gmail.com>
2024-02-18 12:03:23 +0200

    refactor

--- specs/reference-spec.mjs
+++ specs/reference-spec.mjs
@@ -1,4 +1,5 @@
 import { Reference } from '../registry.mjs';
+
 describe('when finding references', () => {
     let A_Id = null;
     let B_Id = null;
@@ -6,49 +7,42 @@ describe('when finding references', () => {
     let A = null;
     let B = null;
     let C = null;
+
     beforeAll(() => {
-        A = Reference.create();
+        A = new Reference();
         ({ Id: A_Id } = A);
-        {
-            B = Reference.create(A_Id);
-            ({ Id: B_Id } = B);
-        }
-        {
-            C = Reference.create(B_Id);
-            ({ Id: C_Id } = C);
-        }
+
+        B = new Reference(A_Id);
+        ({ Id: B_Id } = B);
+
+        C = new Reference(B_Id);
+        ({ Id: C_Id } = C);
     });
+
     it('B should reference A', () => {
-        const isRef = B.references(Id => Id === A_Id);
-        expect(isRef).toBeDefined();
-        expect(isRef).not.toBeNull();
+        const isRef = B.isAssociatedWith(A_Id);
         expect(isRef).toBeTrue();
     });
+
     it('A should reference B', () => {
-        const isRef = A.references(Id => Id === B_Id);
-        expect(isRef).toBeDefined();
-        expect(isRef).not.toBeNull();
+        const isRef = A.isAssociatedWith(B_Id);
         expect(isRef).toBeTrue();
     });
+
     it('B should reference C', () => {
-        const isRef = B.references(Id => Id === C_Id);
-        expect(isRef).toBeDefined();
-        expect(isRef).not.toBeNull();
+        const isRef = B.isAssociatedWith(C_Id);
         expect(isRef).toBeTrue();
     });
+
     it('C should reference B', () => {
-        const isRef = C.references(Id => Id === B_Id);
-        expect(isRef).toBeDefined();
-        expect(isRef).not.toBeNull();
+        const isRef = C.isAssociatedWith(B_Id);
         expect(isRef).toBeTrue();
     });
-    it('should enumerate all references', () => {
-        let Ids = [];
-        while (Reference.next) {
-            Ids.push(Reference.current);
-        }
-        expect(Ids).toBeDefined();
-        expect(Ids).not.toBeNull();
-        expect(Ids.length).toBe(3);
+
+    it('should return an iterator over all references', () => {
+        const referenceIds = Array.from(Reference.nextReference());
+        expect(referenceIds).toContain(A_Id);
+        expect(referenceIds).toContain(B_Id);
+        expect(referenceIds).toContain(C_Id);
     });
 });

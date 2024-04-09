f40e018ddf5726c96e80859eb04dc2eeeb60596d
Marchuan Van Der Merwe <marchuanv@sahomeloans.com>
2024-04-09 20:22:05 +0200

    refactor

--- registry.mjs
+++ registry.mjs
@@ -6,13 +6,12 @@ import { General } from './lib/general.mjs';
 
 import { fileURLToPath, pathToFileURL } from 'url';
 import vm from "vm";
-import { Bag } from './lib/bag.mjs';
 import { Import } from './lib/import.mjs';
 import { NULL, Reflection, UNDEFINED } from './lib/reflection.mjs';
 import { SecureContext } from './lib/secure-context.mjs';
-import { Schema } from './lib/schema.mjs';
 import { Security } from './lib/security.mjs';
 import { UUID } from './lib/uuid.mjs';
+
 
 const importExtended = new Import();
 const security = new Security();
@@ -23,15 +22,22 @@ export { Buffer } from 'node:buffer';
 export { constants, createHash, createHmac, generateKeyPairSync, privateDecrypt, publicEncrypt, randomBytes, randomUUID } from 'node:crypto';
 export { existsSync, lstatSync, readFileSync, readdirSync, statSync, writeFileSync } from 'node:fs';
 export { basename, dirname, extname, join, relative, resolve } from 'node:path';
+export { DataSchema } from './lib/export-a/data-schema.mjs';
+export { Properties, Property } from './lib/export-a/properties.mjs';
+export { Bag } from './lib/export-b/bag.mjs';
+export { TypeInfo } from './lib/export-b/type-info.mjs';
+export { ANY, Type, UNKNOWN } from './lib/export-b/type.mjs';
+export { TypeInfoSchema } from './lib/export-c/type-info-schema.mjs';
+export { TypeMemberInfo } from './lib/export-c/type-member-info.mjs';
+export { Schema } from './lib/export-d/schema.mjs';
+export { TypeMemberInfoSchema } from './lib/export-e/type-member-info-schema.mjs';
+export { TypeSchema } from './lib/export-e/type-schema.mjs';
 export { Serialiser } from './lib/serialiser.mjs';
-export { TypeInfo } from './lib/type-info.mjs';
-export { TypeMemberInfo } from './lib/type-member-info.mjs';
-export { ANY, Type, UNKNOWN } from './lib/type.mjs';
 
 setMetaSchemaOutputFormat(VERBOSE);
 
 export {
-    Bag, General, Jasmine, NULL, Reflection, Schema, SecureContext, UNDEFINED, UUID, VERBOSE, fileURLToPath, importExtended,
+    General, Jasmine, NULL, Reflection, SecureContext, UNDEFINED, UUID, VERBOSE, fileURLToPath, importExtended,
     pathToFileURL, registerSchema, security, sha1, validate as validateSchema,
     vm
 };
--- specs/schema-spec.mjs
+++ specs/schema-spec.mjs
@@ -1,6 +1,5 @@
-import { Schema, TypeInfo } from '../registry.mjs';
-describe('when creating a schema', () => {
-    const expectedErrorMessage = 'The properties argument is null, undefined, not an array, an empty array or contains invalid elements.';
+import { Schema, TypeInfoSchema } from '../registry.mjs';
+fdescribe('when creating a schema', () => {
     it('should raise an error if targeting schema directly', () => {
         try {
             new Schema();
@@ -10,193 +9,124 @@ describe('when creating a schema', () => {
             expect(error.message).toBe('Schema is an abstract class.');
         }
     });
-    it('should raise an error if the properties argument is null', () => {
-        class TestSchema extends Schema { }
+    it('should raise an error if the schema does not have any properties', () => {
+        class NoPropTestSchema extends Schema { }
         try {
-            new TestSchema(null);
+            const testSchema = new NoPropTestSchema();
+            testSchema.validate({
+                message: 'something'
+            });
             fail('expected an error');
         } catch (error) {
             console.log(error);
-            expect(error.message).toBe(expectedErrorMessage);
-        }
-    });
-    it('should raise an error if the properties argument is undefined', () => {
-        class TestSchema extends Schema { }
-        try {
-            new TestSchema();
-            fail('expected an error');
-        } catch (error) {
-            console.log(error);
-            expect(error.message).toBe(expectedErrorMessage);
-        }
-    });
-    it('should raise an error if the properties argument is not an array', () => {
-        class TestSchema extends Schema { }
-        try {
-            new TestSchema({});
-            fail('expected an error');
-        } catch (error) {
-            console.log(error);
-            expect(error.message).toBe(expectedErrorMessage);
-        }
-    });
-    it('should raise an error if the properties argument is array with invalid elements', () => {
-        class TestSchema extends Schema { }
-        try {
-            new TestSchema([{}]);
-            fail('expected an error');
-        } catch (error) {
-            console.log(error);
-            expect(error.message).toBe(expectedErrorMessage);
-        }
-        try {
-            new TestSchema([{ invalidProp: '' }]);
-            fail('expected an error');
-        } catch (error) {
-            console.log(error);
-            expect(error.message).toBe(expectedErrorMessage);
-        }
-        try {
-            new TestSchema([{ name: String, typeInfo: {} }]);
-            fail('expected an error');
-        } catch (error) {
-            console.log(error);
-            expect(error.message).toBe(expectedErrorMessage);
+            expect(error.message).toBe('NoPropTestSchema does not have any properties.');
         }
     });
     it('should raise an error when validating and undefined is passed for verification', () => {
-        class TestSchema extends Schema { }
+        class TestSchemaA extends Schema {
+            get message() {
+                return new TypeInfoSchema(String);
+            }
+        }
         try {
-            const schema = new TestSchema([{ name: 'somekey', typeInfo: new TypeInfo(String) }]);
+            const schema = new TestSchemaA();
             schema.validate(undefined);
             fail('expected an error');
         } catch (error) {
             console.log(error);
-            expect(error.message).toBe(`The verify argument is null, undefined or not an object.`);
+            expect(error.message).toBe(`data to validate is null, undefined, not an object or an empty object.`);
         }
     });
     it('should raise an error when validating and null is passed for verification.', () => {
-        class TestSchema extends Schema { }
+        class TestSchemaB extends Schema {
+            get message() {
+                return new TypeInfoSchema(String);
+            }
+        }
         try {
-            const schema = new TestSchema([{ name: 'somekey', typeInfo: new TypeInfo(String) }]);
+            const schema = new TestSchemaB();
             schema.validate(null);
             fail('expected an error');
         } catch (error) {
             console.log(error);
-            expect(error.message).toBe(`The verify argument is null, undefined or not an object.`);
+            expect(error.message).toBe(`data to validate is null, undefined, not an object or an empty object.`);
         }
     });
     it('should raise an error when validating and a string is passed for verification', () => {
-        class TestSchema extends Schema { }
+        class TestSchemaC extends Schema {
+            get message() {
+                return new TypeInfoSchema(String);
+            }
+        }
         try {
-            const schema = new TestSchema([{ name: 'somekey', typeInfo: new TypeInfo(String) }]);
+            const schema = new TestSchemaC();
             schema.validate('awdwad');
             fail('expected an error');
         } catch (error) {
             console.log(error);
-            expect(error.message).toBe(`The verify argument is null, undefined or not an object.`);
+            expect(error.message).toBe(`data to validate is null, undefined, not an object or an empty object.`);
         }
     });
     it('should raise an error when validating and a number is passed for verification', () => {
-        class TestSchema extends Schema { }
+        class TestSchemaD extends Schema {
+            get message() {
+                return new TypeInfoSchema(String);
+            }
+        }
         try {
-            const schema = new TestSchema([{ name: 'somekey', typeInfo: new TypeInfo(String) }]);
+            const schema = new TestSchemaD();
             schema.validate(0);
             fail('expected an error');
         } catch (error) {
             console.log(error);
-            expect(error.message).toBe(`The verify argument is null, undefined or not an object.`);
+            expect(error.message).toBe(`data to validate is null, undefined, not an object or an empty object.`);
         }
     });
     it('should raise an error when validating and an empty object is passed for verification', () => {
-        class TestSchema extends Schema { }
+        class TestSchemaE extends Schema {
+            get message() {
+                return new TypeInfoSchema(String);
+            }
+        }
         try {
-            const schema = new TestSchema([{ name: 'somekey', typeInfo: new TypeInfo(String) }]);
+            const schema = new TestSchemaE();
             schema.validate({});
             fail('expected an error');
         } catch (error) {
             console.log(error);
-            expect(error.message).toBe(`data to verify is null, undefined or not an object.`);
-        }
-    });
-    it('should raise an error when validating and only a key is passed for verification', () => {
-        class TestSchema extends Schema { }
-        try {
-            const schema = new TestSchema([{ name: 'somekey', typeInfo: new TypeInfo(String) }]);
-            schema.validate({ key: 'somekey ' });
-            fail('expected an error');
-        } catch (error) {
-            console.log(error);
-            expect(error.message).toBe(`data to verify is null, undefined or not an object.`);
-        }
-    });
-    it('should raise an error when validating and an obj with no properties are passed for verification', () => {
-        class TestSchema extends Schema { }
-        try {
-            const schema = new TestSchema([{ name: 'somekey', typeInfo: new TypeInfo(String) }]);
-            const data = {};
-            schema.validate({ data });
-            fail('expected an error');
-        } catch (error) {
-            console.log(error);
-            expect(error.message).toBe(`data does not have the somekey property.`);
-        }
-    });
-    it('should NOT raise an error if the properties argument is array with valid elements', () => {
-        class TestSchema extends Schema { }
-        try {
-            new TestSchema([{ name: 'somekey', typeInfo: new TypeInfo(String) }]);
-        } catch (error) {
-            console.log(error);
-            fail('did not expect any errors');
+            expect(error.message).toBe(`data to validate is null, undefined, not an object or an empty object.`);
         }
     });
     it('should NOT raise an error when validating and passing an obj with properties for verification', () => {
-        class TestSchema extends Schema { }
+        class TestSchemaF extends Schema {
+            get name() {
+                return new TypeInfoSchema(String);
+            }
+            get surname() {
+                return new TypeInfoSchema(String);
+            }
+        }
         try {
-            const schema = new TestSchema([{ name: 'somekey', typeInfo: new TypeInfo(String) }]);
-            schema.validate({ data: { somekey: 'a string' } });
+            const schema = new TestSchemaF();
+            schema.validate({ name: 'Joe', surname: 'Blogs', addresses: ['24 Romsey Grove', '173b Vaus Road'] });
         } catch (error) {
             console.log(error);
             fail('did not expect any errors');
         }
     });
     it('should create an empty object from a schema', () => {
-        class TestSchema extends Schema { }
-        try {
-            const typeInfo = new TypeInfo(String);
-            const schema = new TestSchema([{ name: 'message', typeInfo }]);
-            const data = schema.default;
-            schema.validate({ data });
-        } catch (error) {
-            console.log(error);
-            fail('did not expect any errors');
-        }
-    });
-    it('should NOT raise an error when validating and passing a key and/or an obj with properties for verification', () => {
-        class TestSchema extends Schema { }
-        class Address {
-            constructor() {
-                this.text = 'Vause Road';
+        class TestSchemaG extends Schema {
+            get name() {
+                return new TypeInfoSchema(String);
+            }
+            get surname() {
+                return new TypeInfoSchema(String);
             }
         }
         try {
-            const schema = new TestSchema([{ name: 'somekey', typeInfo: new TypeInfo(Object) }]);
-            schema.validate({ key: 'somekey', data: { somekey: { message: 'Hello World' } } });
-        } catch (error) {
-            console.log(error);
-            fail('did not expect any errors');
-        }
-        try {
-            const schema = new TestSchema([{ name: 'addresses', typeInfo: new TypeInfo([Address]) }]);
-            schema.validate({ data: { addresses: [new Address()] } });
-        } catch (error) {
-            console.log(error);
-            fail('did not expect any errors');
-        }
-        try {
-            const schema = new TestSchema([{ name: 'addresses', typeInfo: new TypeInfo(Array) }]);
-            schema.validate({ key: 'addresses', data: { addresses: ['Vause Road'] } });
+            const schema = new TestSchemaG();
+            const data = schema.default;
+            schema.validate(data);
         } catch (error) {
             console.log(error);
             fail('did not expect any errors');
